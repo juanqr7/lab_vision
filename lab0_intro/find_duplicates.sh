b9c567627789960d4cd20dd9dec9de891fbe20b5
juanqr7 <juanqr7@hotmail.com>
2015-02-03 12:38:08 +0000

    Update find_duplicates.sh

--- lab0_intro/find_duplicates.sh
+++ lab0_intro/find_duplicates.sh
@@ -1,7 +1,8 @@
 
-cksum *.tiff | sort | uniq -d
+cksum *.tiff | cut -f 1 -d' ' | sort | uniq -d
 
 This comand is used on the folder containing all picture files. cksum checks the integrity of all files ending by .tiff. The
-output is then sorted and finally command uniq -d prints duplcated lines.  
+identification of the file is in the first column, the info is taken with command cut, the output is then sorted and 
+finally command uniq -d prints duplcated lines.  
 Script based on "How to Sort and Remove Duplicate Photos in Linux" by Charla Schroder.
 Available online on: http://www.linux.com/learn/tutorials/774031-how-to-sort-and-remove-duplicate-photos-in-linux
